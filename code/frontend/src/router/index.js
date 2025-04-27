acb189698173c172a4f96c6acf0b7420956d05b6
lon-yuan <3321180581@qq.com>
2025-04-27 16:23:17 +0800

    增加详情

--- code/frontend/src/router/index.js
+++ code/frontend/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '@/views/Home.vue'
 import Timeline from '@/views/Timeline.vue'
 import Knowledge from '@/views/Knowledge.vue'
 import Login from '../views/Login.vue'
+import Detail from '../views/Detail.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/timeline', component: Timeline },
   { path: '/knowledge', component: Knowledge },
   { path: '/login', component: Login },
+  { path: '/Detail', component: Detail },
 ]
 
 const router = createRouter({
